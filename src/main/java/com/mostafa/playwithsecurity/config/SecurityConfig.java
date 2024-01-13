352f77fb9c6037f25c67b2dd6f611bd3e6201c6b
m0stafa_darwe4 <mostafadarwesh610@gmail.com>
2024-01-13 21:49:28 +0200

    init commit

--- src/main/java/com/mostafa/playwithsecurity/config/SecurityConfig.java
+++ src/main/java/com/mostafa/playwithsecurity/config/SecurityConfig.java
@@ -1,57 +1,100 @@
 package com.mostafa.playwithsecurity.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.JWKSecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import java.util.UUID;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(authorizeRequest -> authorizeRequest.anyRequest().authenticated())
-                .httpBasic(withDefaults());
 
-        /*httpSecurity.authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.requestMatchers("/demo").hasAuthority("read")
-                                .requestMatchers("/hello").authenticated()
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain asSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());
+
+        http.exceptionHandling(
+                e -> e.authenticationEntryPoint(
+                        new LoginUrlAuthenticationEntryPoint("/login")
                 )
-                .httpBasic(withDefaults());*/
-        return httpSecurity.build();
+        );
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain appSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(formLogin-> formLogin.loginPage("/login")
+                        .defaultSuccessUrl("/home",true))
+                .authorizeHttpRequests(auth->auth.anyRequest().authenticated());
+
+        return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        var u1 = User.withUsername("john")
-                .password(passwordEncoder().encode("12345"))
+        var u1 = User.withUsername("user")
+                .password("password")
                 .authorities("read")
                 .build();
 
-        var u2 = User.withUsername("bill")
-                .password(passwordEncoder().encode("12345"))
-                .authorities("write")
-                .build();
-
-        InMemoryUserDetailsManager uds = new InMemoryUserDetailsManager();
-
-        uds.createUser(u1);
-        uds.createUser(u2);
-
-        return uds;
+        return new InMemoryUserDetailsManager(u1);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();  // NoOpPasswordEncoder ---> marked as deprecated beacause you can ONLY use it in demos.
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient r1 = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("client")
+                .clientSecret("secret")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .redirectUri("https://springone.io/authorized")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .build();
+
+        return new InMemoryRegisteredClientRepository(r1);
+    }
+
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder()
+                .build();
     }
 }
